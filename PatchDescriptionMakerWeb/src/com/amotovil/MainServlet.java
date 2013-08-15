70ea8ca821f7f1a8da2dcb5cdc8c80801007c2b3
Alexander <sanchism@yandex.ru>
2013-08-15 15:06:02 +0400

    second commit

--- PatchDescriptionMakerWeb/src/com/amotovil/MainServlet.java
+++ PatchDescriptionMakerWeb/src/com/amotovil/MainServlet.java
@@ -18,7 +18,7 @@ public class MainServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Servlet works");
+		out.println("Servlet workss");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
